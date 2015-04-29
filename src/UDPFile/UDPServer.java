024e4ea9c248925f49dfb1fa72d054fe3ed2347b
Emily Elmseld <emily.elmseld@gmail.com>
2015-04-29 14:50:09 +0200

    små fix

--- src/UDPFile/UDPServer.java
+++ src/UDPFile/UDPServer.java
@@ -11,12 +11,12 @@ public class UDPServer {
 
 	public static void main(String args[]) throws IOException, SocketException {
 		DataInputStream dis = new DataInputStream(System.in);
-		File file = new File("/Users/emilyelmseld/Documents/fil-lista/");
+		File file = new File("/Users/emilyelmseld/Documents/fil-lista/tidsplan-emily.elmseld.pdf/");
 		FileInputStream fis = new FileInputStream(file);
 		byte[] b = new byte[(int)file.length()];
 		fis.read(b,0, b.length);
-		DatagramSocket ds = new DatagramSocket(1000);
-		DatagramPacket dp = new DatagramPacket(b, b.length, InetAddress.getLocalHost(),2000);
+		DatagramSocket ds = new DatagramSocket(2000);
+		DatagramPacket dp = new DatagramPacket(b, b.length, InetAddress.getByName("192.168.1.15"),2000);
 		ds.send(dp);
 		
 	}
