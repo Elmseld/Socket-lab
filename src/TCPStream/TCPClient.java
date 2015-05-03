2b18595edf9fbcea3bbde34e9ba35d8a5ea95dd6
Elmseld <emily.elmseld@gmail.com>
2015-05-03 13:52:08 +0200

    Första försöket på TCPStream

--- src/TCPStream/TCPClient.java
+++ src/TCPStream/TCPClient.java
@@ -2,18 +2,38 @@
  * 
  */
 package TCPStream;
+import java.net.*;
+import java.io.*;
 
 /**
  * @author Emily Elmseld 28 apr 2015
  *
  */
+
 public class TCPClient {
-
-	/**
-	 * 
-	 */
-	public TCPClient() {
-		// TODO Auto-generated constructor stub
-	}
-
+    public static void receive( String serverIP, int serverPort ) {
+        Socket socket = null;
+        try {
+            InetAddress adress = InetAddress.getByName( serverIP );
+            socket = new Socket( adress , serverPort ); // koppla upp
+            ObjectInputStream input = new ObjectInputStream( 
+                                              socket.getInputStream() );
+            String message;
+            while( true ) {
+                message = input.readUTF(); // här väntar tråden
+                System.out.println( message );
+            }
+        } catch(IOException e) {
+            System.out.println( e );
+        }       
+        try {
+            socket.close(); // avsluta Socket-objektet
+        } catch( Exception e ) {
+            System.out.println( e );
+        }
+    }
+    
+    public static void main( String[] args ) {
+        TCPClient.receive( "192.168.1.9", 5555 );
+    }
 }
--- src/TCPStream/TCPServer.java
+++ src/TCPStream/TCPServer.java
@@ -2,18 +2,45 @@
  * 
  */
 package TCPStream;
+import java.net.*;
+import java.io.*;
 
 /**
  * @author Emily Elmseld 28 apr 2015
  *
  */
+
+
+
 public class TCPServer {
-
-	/**
-	 * 
-	 */
-	public TCPServer() {
-		// TODO Auto-generated constructor stub
-	}
-
+    
+    public static void send( int port, String[] messages ) {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        ObjectOutputStream output;
+        try {
+            serverSocket = new ServerSocket( port );
+            socket = serverSocket.accept();
+            output = new ObjectOutputStream( socket.getOutputStream() );
+            for( int i = 0; i < messages.length; i++ ) {
+                output.writeUTF( messages[ i ] );
+                output.flush();
+                Thread.sleep( 1000 ); // paus 3 sekunder
+            }
+        } catch( Exception e1 ) { // IOException eller InterruptedException
+            System.out.println( e1 );
+        } 
+        try {
+            socket.close();
+            serverSocket.close();
+        } catch( Exception e ) {}
+    }
+    
+    public static void main( String[] args ) {
+        String[] meddelanden = { "Veni, vidi, vici", 
+                                 "Jag kom, jag såg, jag segrade",
+        "Alea iacta est", "Tärningen är kastad",
+        "Et tu Brute", "Även du, min käre Brutus" };
+        TCPServer.send( 5555, meddelanden );
+    }
 }
