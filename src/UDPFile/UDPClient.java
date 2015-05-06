7a1c25672805df5c283c355b95c1f946282056ef
Elmseld <emily.elmseld@gmail.com>
2015-05-06 11:26:07 +0200

    Kommenterat båda UDPKlienterna

--- src/UDPFile/UDPClient.java
+++ src/UDPFile/UDPClient.java
@@ -3,7 +3,6 @@ package UDPFile;
 import java.io.*;
 import java.net.*;
 import java.util.Random;
-import java.util.Scanner;
 
 /**
  * 
@@ -11,68 +10,79 @@ import java.util.Scanner;
 
 /**
  * @author Emily Elmseld 28 apr 2015
- *
+ * Klass som skickar en request till en serverklass och tar 
+ * emot en fil från serverklassen och sparar ner denna och 
+ * ger den ett namn
  */
 public class UDPClient {
 	
 	  public static void main(String[] args) {
+		  //Skapar variabeln socket av typen DatagramSocket med värdet null
 		  DatagramSocket socket = null;
+		  //Sätter portnummret till 12000
 		  int port = 12000;
-		  String s;
-		  byte[] b = new byte[256];
+		  //Skapar variabeln bytes av typen byte[] med värdet 256 bytes
+		  byte[] bytes = new byte[256];
 
-		  
-//		  BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
-		  
+		  		  
 		  try {
+			  //Sätter variabeln socket till en ny datagramSocket;
 			  socket = new DatagramSocket();
-			  
+			  //Skapar en lokal variabel host av typen  InetAddress och sätter den till servern ip-nummer
 			  InetAddress host = InetAddress.getByName("192.168.1.9");
 			  
+			  //while-loop som tar hand om kommunikationen mellan klient och server 
 			  while(true) {
-				  DatagramPacket dp = new DatagramPacket(b, b.length, host, port);
-				  socket.send(dp);
+				  //Skapar variabeln requestDP av typer DatagramPacket med fyra parametrar, bytes, bytes.length, host och port
+				  DatagramPacket requestDP = new DatagramPacket(bytes, bytes.length, host, port);
+				  //Skickar requesten till servern
+				  socket.send(requestDP);
 				  
-				  //now receive reply
-				  //buffer to receive incoming data
+				  
+			      // Skapar en variabel buffer av typen byte-array med en storlek på 65536 byte
 				  byte[] buffer = new byte[65536];
+				  // Skapar ett nytt DatagramPacket med parametrarna data och data.lenght och knyter dem till variabeln packet
 				  DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+		          // Tar emot paket från servern
 				  socket.receive(reply);
 				  
+			      // Skapar en variabel buffer av typen byte-array och lägger in datan från serverpacket
 				  byte[] data = reply.getData();
-				  s = new String(data, 0 , reply.getLength());
-				  
-				  	int p = randInt(0, 500);
-				    FileOutputStream fos = new FileOutputStream("bixie" + p + ".pdf");
-				    BufferedOutputStream bos = new BufferedOutputStream(fos);
-				    bos.write(data, 0, reply.getLength());
-				    bos.close();
+				  // Skapar en variabel diff av typen int som tar emot ett tal från metoden randInt(min, max) 
+				  int diff = randInt(0, 500);
+				  // Skapar en ny fil med namnet bixie + diff-nummret + .pdf
+				  FileOutputStream fos = new FileOutputStream("bixie" + diff + ".pdf");
+				  // Skapar en ny variabel bos av typen BufferedOutputStream med en parameter till den nya filen som skapades i förra raden
+				  BufferedOutputStream bos = new BufferedOutputStream(fos);
+				  // Lägger in datan från server i den nya filen och anger längden
+				  bos.write(data, 0, reply.getLength());
+				  // Stänger BufferedOutputStream
+				  bos.close();
 
 				    
-				  //echo the details of incoming data - client ip : client port - client message
-				  System.out.println(reply.getAddress().getHostAddress() + " : " + reply.getPort() + " - färdig");
+				  //Skriver ut meddelandet i terminalen i formatet,  Klientens ip-adress : Klienten portnr - namnet på filen som sparas - färdig
+				  System.out.println(reply.getAddress().getHostAddress() + " : " + reply.getPort() + " bixie" + diff + ".pdf - färdig");
 				  //För att avbryta mottagningen av filer.
 				  socket.close();
 
 			  }
 		  }
+	      // Fångar och skriver ut problem som kan uppstå i kommunikationen mellan klient-server 
 		  catch(IOException e) {
 			  System.err.println("IOException " + e);
 		  }
 		  
 	}
 
-
-public static int randInt(int min, int max) {
-
-    // NOTE: Usually this should be a field rather than a method
-    // variable so that it is not re-seeded every call.
-    Random rand = new Random();
-
-    // nextInt is normally exclusive of the top value,
-    // so add 1 to make it inclusive
-    int randomNum = rand.nextInt((max - min) + 1) + min;
-
-    return randomNum;
-}
+		/**
+		 * Metod som skapar ett random nummer mellan variablarna min och max för att skapa olika namn på de nedsparade filerna.
+		 * @param min det minsta värdet
+		 * @param max det högsta värdet
+		 * @return returnerar randomNum, de talet som slumpats fram
+		 */
+		public static int randInt(int min, int max) {
+		    Random rand = new Random();
+		    int randomNum = rand.nextInt((max - min) + 1) + min;
+		    return randomNum;
+		}
 }
--- src/UDPStream/UDPClient.java
+++ src/UDPStream/UDPClient.java
@@ -7,38 +7,70 @@ import java.io.*;
 /**
  * @author Emily Elmseld 28 apr 2015
  *
+ * Klassens tar emot string-paket och skriver ut detta i terminalfönster ihop 
+ * med information om serverns Ip-adress och antalet bytes som 
+ * strängen/paketen innehåller.
+ *  
  */
 	public class UDPClient implements Runnable{
+		//Skapar den privata variabeln port av typen int
 		private int port;
 		
+		/**
+		 * En konstruktor med en parameter, port som tar emot ett portnummer
+		 * @param port
+		 */
 		public UDPClient (int port) {
 			this.port = port;
 		}
 	   
-		
+		/**
+		 * Metoden som pratar med servern, skickar en förfrågan och tar emot string-paket och skriver ut innehållet i dem i terminalen
+		 */
 		public void run () {
 	      try {
+	    	 // Skapar ett nytt DatagramSocket-objekt socket med en parameter, port
 	         DatagramSocket socket = new DatagramSocket( port );
+	         // Skapar ett nytt DatagramPacket packet
 	         DatagramPacket packet;
+	         // Skapar variabeln message av typen String
 	         String message;
+	         //Skapar en variabel data av typen byte-array med en storlek på 65536 byte
 	         byte[] data = new byte[ 65536 ];
+	         //While-loop som tar hand om svaren från servern
 	         while( true ) {
+	        	 // skapar ett nytt DatagramPacket med parametrarna data och data.lenght och knyter dem till variabeln packet
 	            packet = new DatagramPacket( data, data.length );
+	            //Tar emot paket från servern
 	            socket.receive( packet );
+	            //Lägger in informationen data och bytestorleken från packet-variabel i message-variabeln
 	            message=new String(packet.getData(),0,packet.getLength(),"ISO-8859-1");
-	            System.out.println( packet.getAddress().toString() + ": " + message + " : " + message.length() + " bytes"); // Skriver ut meddelandet
+	            // Skriver ut meddelandet i terminalen i formatet, ip-adress: medelandet : (medelandets längd i byte) bytes  
+	            System.out.println( packet.getAddress().toString() + ": " + message + " : " + message.length() + " bytes"); 
 	         }
+	         // Fångar och skriver ut problem som kan uppstå i kommunikationen mellan klient-server 
 	      } catch(IOException e) {
 	         System.out.println( e );
 	      }
 	   }
 		
+		/**
+		 * Metod med en parammeter, port som skapar ett nytt UDPClient-objekt, reciver och en ny tråd med variabelnamnet thread
+		 * @param port tar emot ett portnummer här 4444
+		 */
 		public static void receive( int port) {
+			//Skapar ett nytt UDPClient-objekt, receiver som en referens till UDPClient-konstruktorn med en variabel, port 
 			UDPClient receiver = new UDPClient ( port );
+			//Skapar en ny tråd med variabelnamnet thread som tar med sig variabeln receiver
 			Thread thread = new Thread( receiver);
+			//Startar tråden
 			thread.start();
 		}
 	   
+		/**
+		 * Metod som sätter portnumret, här till 4444
+		 * @param args
+		 */
 	   public static void main( String[] args ) {
 		   UDPClient.receive( 4444 );
 	   }
